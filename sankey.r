2ab251939ad7f64997ef3b4add765d057bbf1279
dbendet <dbendet@gmail.com>
2014-08-25 11:44:11 -0400

    updating sankey charts

--- sankey.r
+++ sankey.r
@@ -1,26 +1,46 @@
-require(rCharts)
-require(rjson)
+library(rCharts)
+library(rjson)
 
-x = read.csv('sankey.csv', header=FALSE)
+df = read.csv('sankey.csv', header=FALSE)
 
-colnames(x) <- c("source", "target", "value")
+colnames(df) <- c("source", "target", "value")
 
 sankeyPlot <- rCharts$new()
 
 sankeyPlot$set(
-  data = x,
-  nodeWidth = 15,
-  nodePadding = 10,
-  layout = 32,
-  width = 500,
-  height = 300,
-  units = "TWh",
-  title = "Sankey Diagram"
+ data = df,
+ nodeWidth = 15,
+ nodePadding = 10,
+ layout = 32,
+ width = 500,
+ height = 300,
+ units = "TWh",
+ title = "Sankey Diagram"
 )
 
 sankeyPlot$setLib('http://timelyportfolio.github.io/rCharts_d3_sankey')
 
-sankeyPlot
+sankeyPlot$setTemplate(
+  afterScript = "
+    <script>
+    //  var cscale = d3.scale.category10();
+        var cscale = d3.scale.ordinal().range(['#27ae60', '#2980b9', '#e67e22', '#f1c40f', '#e74c3c']);
 
+      d3.selectAll('#{{ chartId }} svg path.link')
+        .style('stroke', function(d){
+          return cscale(d.source.name);
+        })
+
+       // .style('stroke-opacity', .7)
+
+      d3.selectAll('#{{ chartId }} svg .node rect')
+        .style('fill', function(d){
+          return cscale(d.name)
+        })
+        .style('stroke', 'none')
+    </script>
+")
+
+sankeyPlot
 
 
